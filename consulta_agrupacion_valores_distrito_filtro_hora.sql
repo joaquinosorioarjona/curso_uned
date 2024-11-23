ce976813e0975b5f068b3c7a445bd477f9921e2f
Joaquín Osorio Arjona <joaosoarj@hotmail.com>
2024-11-23 13:13:12 +0100

    Update consulta_agrupacion_valores_distrito_filtro_hora.sql

--- consulta_agrupacion_valores_distrito_filtro_hora.sql
+++ consulta_agrupacion_valores_distrito_filtro_hora.sql
@@ -5,18 +5,10 @@ CREATE TABLE sum_distritos_15_enero AS
         destino, 
         actividad_destino,
         municipio_destino,
-        SUM(viajes) AS total_poblacion,
-        CASE
-            WHEN hora = 8 THEN '8'
-            WHEN hora = 9 THEN '9'
-        END AS rango_hora
+        SUM(viajes) AS total_poblacion
     FROM 
         viajes_distritos_15_enero
     WHERE 
         hora IN (8, 9)
     GROUP BY 
-        destino, actividad_destino, municipio_destino, 
-        CASE
-            WHEN hora = 8 THEN '8'
-            WHEN hora = 9 THEN '9'
-        END;
+        destino, actividad_destino, municipio_destino;
